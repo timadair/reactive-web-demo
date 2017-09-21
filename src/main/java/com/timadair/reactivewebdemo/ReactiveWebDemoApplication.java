4bafbfe64a8d3fc494fae667700a41d84eec844e
Tim Adair <tymmo.t@gmail.com>
2017-09-21 17:51:31 -0600

    I1nitial commit
    Auto-generated application class and test

---
+++ src/main/java/com/timadair/reactivewebdemo/ReactiveWebDemoApplication.java
@@ -0,0 +1,12 @@
+package com.timadair.reactivewebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWebDemoApplication.class, args);
+	}
+}
