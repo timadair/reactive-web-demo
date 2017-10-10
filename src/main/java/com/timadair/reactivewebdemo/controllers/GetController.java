48a3b044a490fb96c6917b784ffa40bb54e12422
Tim Adair <timadair@familysearch.org>
2017-10-10 17:20:51 -0600

    Add blocking endpoint that uses reactive internally, but does not return a Flux.

---
+++ src/main/java/com/timadair/reactivewebdemo/controllers/GetController.java
@@ -0,0 +1,25 @@
+package com.timadair.reactivewebdemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.List;
+
+/**
+ * Demonstrates the flexibility of Spring Web.
+ *
+ * @author timadair
+ * Created 10/10/17.
+ */
+@RestController
+public class GetController {
+  @GetMapping(value = "/list")
+  public List<String> getList() {
+    return Flux.interval(Duration.ofMillis(50))
+      .take(100) // Take unsubscribes after the 100th event.
+      .map(i -> "count: " + i)
+      .collectList().block(); // This blocks until the take calls onCompleted on its subscribers.
+  }
+}
