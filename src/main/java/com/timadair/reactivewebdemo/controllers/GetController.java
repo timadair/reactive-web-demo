cff63c6af97aa329a1f777eee19ee4077f748cb3
Tim Adair <timadair@familysearch.org>
2017-10-10 18:30:28 -0600

    Add endpoints for many RxJava2 and Reactor types

--- src/main/java/com/timadair/reactivewebdemo/controllers/GetController.java
+++ src/main/java/com/timadair/reactivewebdemo/controllers/GetController.java
@@ -1,11 +1,17 @@
 package com.timadair.reactivewebdemo.controllers;
 
+import io.reactivex.Flowable;
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+import io.reactivex.Single;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Demonstrates the flexibility of Spring Web.
@@ -40,4 +46,50 @@ public class GetController {
     System.out.println(i);
     return "count: " + i;
   }
+
+  @GetMapping(value = "/mono")
+  public Mono<List<String>> getMono() {
+    return Flux.interval(Duration.ofMillis(50))
+      .take(100)
+      .map(i -> "Count: " + i)
+      .collectList();
+  }
+
+  @GetMapping(value = "/flowable", produces = "text/event-stream")
+  public Flowable<String> getFlowable() {
+    return Flowable.interval(50, TimeUnit.MILLISECONDS)
+      .take(100)
+      .map(i -> "Count: " + i);
+  }
+
+  @GetMapping(value = "/observable", produces = "text/event-stream")
+  public Observable<String> getObservable() {
+    return Observable.interval(50, TimeUnit.MILLISECONDS)
+      .take(100)
+      .map(i -> "Count: " + i);
+  }
+
+  @GetMapping(value = "/single")
+  public Single<String> getSingle() {
+    return Flowable.interval(50, TimeUnit.MILLISECONDS)
+      .take(100)
+      .map(i -> "Count: " + i)
+      .elementAt(100, "Exceeded production target");
+  }
+
+  @GetMapping(value = "/maybe")
+  public Maybe<String> getMaybe() {
+    return Observable.interval(50, TimeUnit.MILLISECONDS)
+      .take(100)
+      .map(i -> "Count: " + i)
+      .elementAt(99);
+  }
+
+  @GetMapping(value = "/maybe-not")
+  public Maybe<String> getMissingMaybe() {
+    return Observable.interval(50, TimeUnit.MILLISECONDS)
+      .take(100)
+      .map(i -> "Count: " + i)
+      .elementAt(100);
+   }
 }
